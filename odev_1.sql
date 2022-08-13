b1f05f7681e36b9418ca9a24f6e30cd12f5714f8
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2022-08-13 17:51:50 +0300

    Update odev_1.sql
    3. düzenleme

--- odev_1.sql
+++ odev_1.sql
@@ -2,3 +2,7 @@ SELECT title , description FROM film
 
 SELECT * FROM film
 WHERE length > 60 and length < 75;
+
+
+SELECT * FROM film
+WHERE rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;
