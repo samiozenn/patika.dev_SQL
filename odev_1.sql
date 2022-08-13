2999b95216fcd5db4479b93a2f79b53657c5db26
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2022-08-13 18:00:18 +0300

    Update odev_1.sql

--- odev_1.sql
+++ odev_1.sql
@@ -6,3 +6,10 @@ WHERE length > 60 and length < 75;
 
 SELECT * FROM film
 WHERE rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;
+
+SELECT last_name FROM customer
+WHERE first_name = 'Mary';
+
+SELECT * FROM film
+WHERE length < 50 and (rental_rate = 2.99 or (not rental_rate = 4.99));
+
