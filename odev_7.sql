1889c63106d3fbb70e43b20b930fdf4ae2d62fd7
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2022-09-05 17:49:40 +0300

    Update odev_7.sql

--- odev_7.sql
+++ odev_7.sql
@@ -16,4 +16,14 @@ ORDER BY replacement_cost;
 SELECT COUNT(store_id),store_id FROM customer 
 GROUP BY store_id;
 
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra 
+ --en fazla şehir sayısı barındıran country_id bilgisini 
+ --ve şehir sayısını paylaşınız.
+ 
+SELECT country_id, COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC 
+LIMIT 1;
+
+
 
