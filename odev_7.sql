6d75a2e4ed5e4d8631af803268e5d2e2c892360a
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2022-09-05 17:53:13 +0300

    Update odev_7.sql

--- odev_7.sql
+++ odev_7.sql
@@ -1,7 +1,8 @@
 --film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
 
-SELECT COUNT(rating),rating FROM film 
+SELECT rating FROM film 
 GROUP BY rating;
+
 
 --film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda 
 --film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
